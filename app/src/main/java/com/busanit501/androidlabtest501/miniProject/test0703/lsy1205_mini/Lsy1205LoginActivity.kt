3d396fd77a55dbb7283ebfeeac763ba9504fd687
이상용 <lsy3709@naver.com>
2024-07-03 11:49:16 +0900

    3교시, ch6 뷰 기본 알아보기, 미니실습 안내

---
+++ app/src/main/java/com/busanit501/androidlabtest501/miniProject/test0703/lsy1205_mini/Lsy1205LoginActivity.kt
@@ -0,0 +1,21 @@
+package com.busanit501.androidlabtest501.miniProject.test0703.lsy1205_mini
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.busanit501.androidlabtest501.R
+
+class Lsy1205LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_lsy1205_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ app/src/main/java/com/busanit501/androidlabtest501/miniProject/test0703/test0101_mini/Test0101JoinActivity.kt
@@ -0,0 +1,21 @@
+package com.busanit501.androidlabtest501.miniProject.test0703.test0101_mini
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.busanit501.androidlabtest501.R
+
+class Test0101JoinActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_test0101_join)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
