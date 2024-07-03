c8df461ee84e542c9057f50e089585d421eaf102
KODOKYUN <hwasungnj3@gmail.com>
2024-07-03 14:18:33 +0900

    0703 고도균 화면 구현 미니 프로젝트 제출 합니다.7

--- app/src/main/java/com/busanit501/androidlabtest501/miniProject/test0703/kdk1115_mini/Kdk1115LoginActivity.kt
+++ app/src/main/java/com/busanit501/androidlabtest501/miniProject/test0703/kdk1115_mini/Kdk1115LoginActivity.kt
@@ -11,7 +11,7 @@ class Kdk1115LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_lsy1205_login)
+        setContentView(R.layout.activity_kdk1115_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
