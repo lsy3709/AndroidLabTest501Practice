955234264e2d16259aa3c17312a2e3e33f9a5591
LeeSooJin <gowl0918@gmail.com>
2024-07-03 14:26:09 +0900

    이수진

---
+++ app/src/main/java/com/busanit501/androidlabtest501/miniProject/test0703/lsj0918_mini/Lsj0918SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.busanit501.androidlabtest501.miniProject.test0703.lsj0918_mini
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.busanit501.androidlabtest501.R
+
+class Lsj0918SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_lsj0918_splash)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
