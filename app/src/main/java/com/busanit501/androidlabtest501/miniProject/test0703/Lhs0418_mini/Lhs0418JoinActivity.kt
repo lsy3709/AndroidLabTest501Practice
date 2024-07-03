f92b76c21e8faf7d9cfdee342ec8bc8cab18afe1
LeeHanSol <hslee4954@gmail.com>
2024-07-03 15:14:44 +0900

    어휴 버그;;

---
+++ app/src/main/java/com/busanit501/androidlabtest501/miniProject/test0703/Lhs0418_mini/Lhs0418JoinActivity.kt
@@ -0,0 +1,25 @@
+package com.busanit501.androidlabtest501.miniProject.test0703.Lhs0418_mini
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.busanit501.androidlabtest501.R
+import com.busanit501.androidlabtest501.databinding.ActivityLhs0418JoinBinding
+import com.busanit501.androidlabtest501.databinding.ActivityMjs1003JoinBinding
+
+class Lhs0418JoinActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+//        setContentView(R.layout.activity_lhs0418_join)
+        val binding = ActivityLhs0418JoinBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
