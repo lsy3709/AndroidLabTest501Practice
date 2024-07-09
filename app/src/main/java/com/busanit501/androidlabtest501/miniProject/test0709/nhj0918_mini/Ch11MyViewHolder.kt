5e807ff5662c24695eaa60e2b63a7fa50b8c1386
NaHoJin <ghwls3995@gmail.com>
2024-07-09 12:39:27 +0900

    나호진 이륙을 허가주십쇼.

---
+++ app/src/main/java/com/busanit501/androidlabtest501/miniProject/test0709/nhj0918_mini/Ch11MyViewHolder.kt
@@ -0,0 +1,14 @@
+package com.busanit501.androidlabtest501.miniProject.test0709.nhj0918_mini
+
+import androidx.recyclerview.widget.RecyclerView
+import com.busanit501.androidlabtest501.databinding.Nhj0918ItemSimpleRecyclerBinding
+
+// 순서 2
+// 목록의 요소 뷰를 생성.
+
+// 순서3
+// ch11_item_simple_recycler , 파일 생성, layout 폴더
+//
+class Ch11MyViewHolder(val binding:Nhj0918ItemSimpleRecyclerBinding ):RecyclerView.ViewHolder(binding.root) {
+
+}
