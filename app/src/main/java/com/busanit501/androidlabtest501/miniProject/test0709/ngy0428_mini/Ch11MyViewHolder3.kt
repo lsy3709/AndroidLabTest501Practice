96e7f74d4d2a0e8f40d705007cf01760ebfba5bc
NamGwiYeon <earkite.n@gmail.com>
2024-07-09 12:13:51 +0900

    0709 NGY 리사이클러 뷰 추가 완료 (리사이클러 3 이름 수정)

--- app/src/main/java/com/busanit501/androidlabtest501/miniProject/test0709/ngy0428_mini/Ch11MyViewHolder3.kt
+++ app/src/main/java/com/busanit501/androidlabtest501/miniProject/test0709/ngy0428_mini/Ch11MyViewHolder3.kt
@@ -2,8 +2,8 @@ package com.busanit501.androidlabtest501.miniProject.test0709.ngy0428_mini
 
 import androidx.recyclerview.widget.RecyclerView
 import com.busanit501.androidlabtest501.databinding.Ch11ItemSimpleRecycler2Binding
-import com.busanit501.androidlabtest501.databinding.Ch11ItemSimpleRecycler3Binding
 import com.busanit501.androidlabtest501.databinding.Ch11ItemSimpleRecyclerBinding
+import com.busanit501.androidlabtest501.databinding.NgyCh11ItemSimpleRecycler3Binding
 
-class Ch11MyViewHolder3(val binding: Ch11ItemSimpleRecycler3Binding): RecyclerView.ViewHolder(binding.root) {
+class Ch11MyViewHolder3(val binding: NgyCh11ItemSimpleRecycler3Binding): RecyclerView.ViewHolder(binding.root) {
 }
