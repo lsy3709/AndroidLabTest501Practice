e4d45c323662cff8dff89991902ab2df15144e65
KODOKYUN <hwasungnj3@gmail.com>
2024-07-09 14:28:09 +0900

    0709 고도균 머짓 부탁드립니다.3

--- app/src/main/java/com/busanit501/androidlabtest501/miniProject/test0709/kdk1115_mini/Ch11MyViewHolder3.kt
+++ app/src/main/java/com/busanit501/androidlabtest501/miniProject/test0709/kdk1115_mini/Ch11MyViewHolder3.kt
@@ -2,8 +2,8 @@ package com.busanit501.androidlabtest501.miniProject.test0709.kdk1115_mini
 
 import androidx.recyclerview.widget.RecyclerView
 import com.busanit501.androidlabtest501.databinding.Ch11ItemSimpleRecycler2Binding
-import com.busanit501.androidlabtest501.databinding.Ch11ItemSimpleRecycler3Binding
+import com.busanit501.androidlabtest501.databinding.Kdkch11ItemSimpleRecycler3Binding
 import com.busanit501.androidlabtest501.databinding.Ch11ItemSimpleRecyclerBinding
 
-class Ch11MyViewHolder3(val binding: Ch11ItemSimpleRecycler3Binding): RecyclerView.ViewHolder(binding.root) {
+class Ch11MyViewHolder3(val binding: Kdkch11ItemSimpleRecycler3Binding): RecyclerView.ViewHolder(binding.root) {
 }
