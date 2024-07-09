9b4c46618b53c3693a0a1c1382fb6386b391f645
moonsuyeon1 <suyun8570@gmail.com>
2024-07-09 11:40:18 +0900

    문수연 숙제제출합니다!

--- app/src/main/java/com/busanit501/androidlabtest501/miniProject/test0709/msy0402_mini/Ch11MainRecyclerView1Activity_msy0402.kt
+++ app/src/main/java/com/busanit501/androidlabtest501/miniProject/test0709/msy0402_mini/Ch11MainRecyclerView1Activity_msy0402.kt
@@ -1,4 +1,4 @@
-package com.busanit501.androidlabtest501.miniProject.test0709.lsy1205_mini
+package com.busanit501.androidlabtest501.miniProject.test0709.msy0402_mini
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
@@ -8,10 +8,9 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.busanit501.androidlabtest501.R
 import com.busanit501.androidlabtest501.ch11_jetpack.recyclerview.test1simple.Ch11MyAdapterSample
-import com.busanit501.androidlabtest501.ch11_jetpack.recyclerview.test1simple.Ch11MyAdapterSample2
 import com.busanit501.androidlabtest501.databinding.ActivityCh11MainRecyclerView1Binding
 
-class Ch11MainRecyclerView1Activity : AppCompatActivity() {
+class Ch11MainRecyclerView1Activity_msy0402 : AppCompatActivity() {
     lateinit var binding: ActivityCh11MainRecyclerView1Binding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,44 +19,48 @@ class Ch11MainRecyclerView1Activity : AppCompatActivity() {
         binding = ActivityCh11MainRecyclerView1Binding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        setContentView(R.layout.activity_ch11_main_recycler_view1)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        //샘플 더미 데이터 넣기.
+        // Sample dummy data
         val datas = mutableListOf<String>()
-        for(i in 1..10) {
+        for (i in 1..10) {
             datas.add("오늘 점심 뭐먹지 ? $i")
         }
 
-        //리사이클러뷰 , 어떤 모양으로 배치할지 정하기.
-        // 세로 방향, 가로 방향, 지그재그, 그리드 등.
-        val layoutManager = LinearLayoutManager(this@Ch11MainRecyclerView1Activity)
+        // Set layout manager
+        val layoutManager = LinearLayoutManager(this@Ch11MainRecyclerView1Activity_msy0402)
         layoutManager.orientation = LinearLayoutManager.VERTICAL
         binding.ch11RecyclerSample1.layoutManager = layoutManager
 
-        // 어댑터 붙이기.
+        // Set adapter
         binding.ch11RecyclerSample1.adapter = Ch11MyAdapterSample(datas)
 
-
-        // 2번째 리사이클러뷰 붙이기 작업, 더미 데이터 추가.
-        //샘플 더미 데이터 넣기.2
+        // Sample dummy data 2
         val datas2 = mutableListOf<String>()
-        for(i in 1..10) {
+        for (i in 1..10) {
             datas2.add("오늘 점심 뭐먹지2 ? $i")
         }
 
-        //리사이클러뷰 , 어떤 모양으로 배치할지 정하기.
-        // 세로 방향, 가로 방향, 지그재그, 그리드 등.
-        val layoutManager2 = LinearLayoutManager(this@Ch11MainRecyclerView1Activity)
+        // Set layout manager and adapter for the second RecyclerView
+        val layoutManager2 = LinearLayoutManager(this@Ch11MainRecyclerView1Activity_msy0402)
         layoutManager2.orientation = LinearLayoutManager.VERTICAL
         binding.ch11RecyclerSample2.layoutManager = layoutManager2
+        binding.ch11RecyclerSample2.adapter = Ch11MyAdapterSample(datas2)
 
-        // 어댑터 붙이기.
-        binding.ch11RecyclerSample2.adapter = Ch11MyAdapterSample2(datas2)
+        // Sample dummy data 3
+        val datas3 = mutableListOf<String>()
+        for (i in 1..10) {
+            datas3.add("문수연 숙제제출 ? $i")
+        }
 
-    } //onCreate
+        // Set layout manager and adapter for the third RecyclerView
+        val layoutManager3 = LinearLayoutManager(this@Ch11MainRecyclerView1Activity_msy0402)
+        layoutManager3.orientation = LinearLayoutManager.VERTICAL
+        binding.ch11RecyclerSample3.layoutManager = layoutManager3
+        binding.ch11RecyclerSample3.adapter = Ch11MyAdapterSample(datas3)
+    }
 }
