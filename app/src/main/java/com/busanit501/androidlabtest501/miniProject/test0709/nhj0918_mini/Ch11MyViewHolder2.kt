400c8daef794d28b8f4bbe1e2de6639bfb3ed228
NaHoJin <ghwls3995@gmail.com>
2024-07-09 14:55:35 +0900

    나호진 no error keep going

---
+++ app/src/main/java/com/busanit501/androidlabtest501/miniProject/test0709/nhj0918_mini/Ch11MyViewHolder2.kt
@@ -0,0 +1,7 @@
+package com.busanit501.androidlabtest501.miniProject.test0709.nhj0918_mini
+
+import androidx.recyclerview.widget.RecyclerView
+import com.busanit501.androidlabtest501.databinding.Nhj0918ItemSimpleRecycler2Binding
+
+class Ch11MyViewHolder2(val binding: Nhj0918ItemSimpleRecycler2Binding): RecyclerView.ViewHolder(binding.root) {
+}
