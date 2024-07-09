2988c1a73f6b78708a8043f0979e46ecc1fad8da
kimsoyoung <ggomag3243@gmail.com>
2024-07-09 12:09:12 +0900

    0709 김소영 미니실습

---
+++ app/src/main/java/com/busanit501/androidlabtest501/miniProject/test0709/ksy1028_mini/Ch11MyViewHolder2.kt
@@ -0,0 +1,8 @@
+package com.busanit501.androidlabtest501.miniProject.test0709.ksy1028_mini
+
+import androidx.recyclerview.widget.RecyclerView
+import com.busanit501.androidlabtest501.databinding.Ch11ItemSimpleRecycler2Binding
+import com.busanit501.androidlabtest501.databinding.Ch11ItemSimpleRecyclerBinding
+
+class Ch11MyViewHolder2(val binding: Ch11ItemSimpleRecycler2Binding): RecyclerView.ViewHolder(binding.root) {
+}
