1ed4bcefaea9407c71759eb95563e8cd02bc86e1
이상용 <lsy3709@naver.com>
2024-07-09 14:11:47 +0900

    미니실습안내

--- app/src/main/java/com/busanit501/androidlabtest501/miniProject/test0709/lsy1205_mini/Ch11MainRecyclerView1Activity.kt
+++ app/src/main/java/com/busanit501/androidlabtest501/miniProject/test0709/lsy1205_mini/Ch11MainRecyclerView1Activity.kt
@@ -10,7 +10,7 @@ import com.busanit501.androidlabtest501.R
 import com.busanit501.androidlabtest501.ch11_jetpack.recyclerview.test1simple.Ch11MyAdapterSample
 import com.busanit501.androidlabtest501.ch11_jetpack.recyclerview.test1simple.Ch11MyAdapterSample2
 import com.busanit501.androidlabtest501.databinding.ActivityCh11MainRecyclerView1Binding
-
+//test2
 class Ch11MainRecyclerView1Activity : AppCompatActivity() {
     lateinit var binding: ActivityCh11MainRecyclerView1Binding
 
