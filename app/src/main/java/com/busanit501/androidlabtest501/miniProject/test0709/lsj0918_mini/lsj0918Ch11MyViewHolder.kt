8537d9e4c409fd6e579b1c6fb089717dab0ccbab
LeeSooJin <gowl0918@gmail.com>
2024-07-09 12:30:57 +0900

    이수진

---
+++ app/src/main/java/com/busanit501/androidlabtest501/miniProject/test0709/lsj0918_mini/lsj0918Ch11MyViewHolder.kt
@@ -0,0 +1,9 @@
+package com.busanit501.androidlabtest501.ch11_jetpack.recyclerview.test1simple
+
+import androidx.recyclerview.widget.RecyclerView
+import com.busanit501.androidlabtest501.databinding.Ch11ItemSimpleRecyclerBinding
+
+
+class lsj0918Ch11MyViewHolder(val binding: Ch11ItemSimpleRecyclerBinding):RecyclerView.ViewHolder(binding.root) {
+
+}
---
+++ app/src/main/java/com/busanit501/androidlabtest501/miniProject/test0709/lsj0918_mini/lsj0918Ch11MyViewHolder3.kt
@@ -0,0 +1,11 @@
+package com.busanit501.androidlabtest501.ch11_jetpack.recyclerview.test1simple
+
+import androidx.recyclerview.widget.RecyclerView
+import com.busanit501.androidlabtest501.databinding.Ch11ItemSimpleRecycler2Binding
+import com.busanit501.androidlabtest501.databinding.Ch11ItemSimpleRecyclerBinding
+import com.busanit501.androidlabtest501.databinding.Lsj0918Ch11ItemSimpleRecycler3Binding
+
+
+class lsj0918Ch11MyViewHolder3(val binding: Lsj0918Ch11ItemSimpleRecycler3Binding):RecyclerView.ViewHolder(binding.root) {
+
+}
