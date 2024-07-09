045874e3d1cc40c523ffba77e4f612bfeaedc1af
OhHyunJe <qkqh12311@gmail.com>
2024-07-09 12:30:02 +0900

    오현제입니다. 고생하십니다 -완-

---
+++ app/src/main/java/com/busanit501/androidlabtest501/miniProject/test0709/ohj0106_mini/Ch11MyViewHolder3.kt
@@ -0,0 +1,8 @@
+package com.busanit501.androidlabtest501.miniProject.test0709.ohj0106_mini
+
+import androidx.recyclerview.widget.RecyclerView
+import com.busanit501.androidlabtest501.databinding.Ch11ItemSimpleRecycler2Binding
+import com.busanit501.androidlabtest501.databinding.OhjCh11ItemSimpleRecycler3Binding
+
+class Ch11MyViewHolder3 (val binding: OhjCh11ItemSimpleRecycler3Binding): RecyclerView.ViewHolder(binding.root) {
+}
