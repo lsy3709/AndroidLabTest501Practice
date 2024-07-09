90bc97028b4ed0ed895fa16068eec0e1afa30277
HongJintae <ghdwlsxo33@gmail.com>
2024-07-09 14:54:33 +0900

    미니실습 완료 홍진태 0709-2

--- app/src/main/java/com/busanit501/androidlabtest501/miniProject/test0709/hjt0131_mini/Ch11MyViewHolder3.kt
+++ app/src/main/java/com/busanit501/androidlabtest501/miniProject/test0709/hjt0131_mini/Ch11MyViewHolder3.kt
@@ -1,9 +1,10 @@
 package com.busanit501.androidlabtest501.miniProject.test0709.hjt0131_mini
 
 import androidx.recyclerview.widget.RecyclerView
+import com.busanit501.androidlabtest501.databinding.Ch11Hjt0131ItemSimpleRecycler3Binding
 import com.busanit501.androidlabtest501.databinding.Ch11ItemSimpleRecycler2Binding
-import com.busanit501.androidlabtest501.databinding.Ch11ItemSimpleRecycler3Binding
 
-class Ch11MyViewHolder3(val binding: Ch11ItemSimpleRecycler3Binding): RecyclerView.ViewHolder(binding.root) {
+
+class Ch11MyViewHolder3(val binding: Ch11Hjt0131ItemSimpleRecycler3Binding): RecyclerView.ViewHolder(binding.root) {
 
 }
