59a6717ef3e69d9f652a316a2fe7f85f5086a585
moonsuyeon1 <suyun8570@gmail.com>
2024-07-10 15:47:03 +0900

    문수연 pr 보냅니다!

---
+++ app/src/main/java/com/busanit501/androidlabtest501/miniProject/test0710/msy0402_mini/fragment_ch11_one_msy0402.kt
@@ -0,0 +1,21 @@
+package com.busanit501.androidlabtest501.miniProject.test0710.msy0402_mini
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.busanit501.androidlabtest501.R
+
+class fragment_ch11_one_msy0402 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_fragment_ch11_one_msy0402)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
